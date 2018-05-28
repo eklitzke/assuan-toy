7a6d560676fca6bf69d2788142688826f6026f8d
Evan Klitzke <evan@eklitzke.org>
2018-05-27 19:14:12 -0700

    allow specifying the socket path

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 This is just me playing around with
 [libassuan](https://www.gnupg.org/related_software/libassuan/index.en.html) and
 [gpg-agent](https://www.gnupg.org/index.html).
+
+If you want to compile the code, this is a standard autotools project:
+
+```bash
+$ ./autogen.sh
+$ ./configure
+$ make
+```
+
+This will produce an executable at `src/assuan-toy`.
--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,13 @@
 #include <unistd.h>
 
 #include <assuan.h>
+#include <getopt.h>
 #include <gpg-error.h>
 
 #include <iostream>
 #include <sstream>
+
+#include "./config.h"
 
 class Assuan {
  public:
@@ -59,12 +62,39 @@ std::string GuessSocket() {
 }
 
 int main(int argc, char **argv) {
+  std::string socket_path;
+  static const char short_opts[] = "hs:";
+  static struct option long_opts[] = {{"help", no_argument, 0, 'h'},
+                                      {"socket", required_argument, 0, 's'},
+                                      {0, 0, 0, 0}};
+  for (;;) {
+    int c = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+    if (c == -1) {
+      break;
+    }
+    switch (c) {
+      case 'h':
+        std::cout << PACKAGE_STRING << "\n\n";
+        std::cout << "usage: " << PACKAGE << " [-h] [-s SOCKET]\n";
+        return 0;
+        break;
+      case 's':
+        socket_path = optarg;
+        break;
+      case '?':
+        // getopt_long should already have printed an error message
+        break;
+      default:
+        std::cerr << "unrecognized command line flag: " << optarg << "\n";
+        abort();
+    }
+  }
+
   // initialize libgpg-error
   gpgrt_check_version(nullptr);
 
   Assuan ctx;
-  ctx.SocketConnect(GuessSocket());
-
+  ctx.SocketConnect(socket_path.empty() ? GuessSocket() : socket_path);
   ctx.WriteLine("GETINFO version");
   std::cout << ctx.ReadLine() << "\n";
   return 0;
